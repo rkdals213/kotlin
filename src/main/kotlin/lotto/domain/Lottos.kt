145de9bf6eb8958bde9f550abf2c240bbd842e68
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-11-09 00:00:42 +0900

    a

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -4,28 +4,26 @@ import java.util.stream.Stream
 import kotlin.streams.toList
 
 class Lottos(
-    private val count: Int,
-    manualLottoNumbers: List<List<Int>>
+    private val count: Int, manualLottoNumbers: List<List<Int>>
 ) {
     private val lottos: List<Lotto> = manualLottoNumbers.map { list -> Lotto(list.map { i -> Number(i) }) }
-        .plus(
-            Stream.generate { Lotto() }
-                .limit(countMinusManualSize(manualLottoNumbers))
-                .toList()
-        )
+            .plus(Stream.generate { Lotto() }
+                    .limit(countMinusManualSize(manualLottoNumbers))
+                    .toList())
 
-    private fun countMinusManualSize(manualLottoNumbers: List<List<Int>>) =
-        count.toLong() - manualLottoNumbers.size
-
-    fun lottos(): List<Lotto> {
-        return lottos
+    init {
+        require(totalCountOverTheManualSize(manualLottoNumbers)) {
+            "수동 입력한 로또 번호의 갯수가 전체 구매 갯수보다 작거나 같아야합니다"
+        }
     }
 
-    fun count(): Int {
-        return count
-    }
+    private fun totalCountOverTheManualSize(manualLottoNumbers: List<List<Int>>) = countMinusManualSize(manualLottoNumbers) >= 0
 
-    fun cost(): Int {
-        return lottos.sumOf { lotto -> lotto.cost() }
-    }
+    private fun countMinusManualSize(manualLottoNumbers: List<List<Int>>) = count.toLong() - manualLottoNumbers.size
+
+    fun lottos() = lottos
+
+    fun count() = count
+
+    fun cost() = lottos.sumOf { lotto -> lotto.cost() }
 }
