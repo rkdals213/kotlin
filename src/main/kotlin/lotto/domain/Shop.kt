8f46467b9471d2cfb3cb73a6df154df9f1396ae0
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-11-08 23:15:33 +0900

    a

--- src/main/kotlin/lotto/domain/Shop.kt
+++ src/main/kotlin/lotto/domain/Shop.kt
@@ -5,12 +5,25 @@ class Shop {
         private const val COST = 1000
 
         fun buyLotto(money: Int, manualLottoNumbers: List<List<Int>>): Lottos {
+            require(moneyIsPositive(money)) {
+                "잘못된 금액을 입력하셨습니다"
+            }
+
             val count = calculateCount(money)
+            check(moneyOverManualLottoCount(manualLottoNumbers, count)) {
+                "구매한 금액보다 더 많은 수동 로또를 입력하였습니다"
+            }
+
             return Lottos(count, manualLottoNumbers)
         }
 
-        private fun calculateCount(money: Int): Int {
-            return money / COST
-        }
+        private fun moneyIsPositive(money: Int) =
+            money >= 0
+
+        private fun moneyOverManualLottoCount(manualLottoNumbers: List<List<Int>>, count: Int) =
+            manualLottoNumbers.size <= count
+
+        private fun calculateCount(money: Int) =
+            money / COST
     }
 }
