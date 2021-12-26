2b9857d8a9744735c16e285bbe0d00cb01581fcc
rkdals213 <rkdals213@naver.com>
2021-12-26 17:26:39 +0900

    a

---
+++ src/test/kotlin/reference/CollectionTests.kt
@@ -0,0 +1,20 @@
+package reference
+
+import org.junit.jupiter.api.Test
+
+class CollectionTests {
+
+    @Test
+    fun test01() {
+        val list1: List<String> = listOf("a", "b", "c", "d", "e")
+        val list2: List<String> = listOf("c", "e", "f")
+
+        val set1 = list1 intersect list2
+        val set2 = list1 subtract list2
+        set1.printAllElement()
+        println()
+        set2.printAllElement()
+    }
+
+    private fun Collection<String>.printAllElement() = forEach { print("$it ") }
+}
